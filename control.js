b2c55076dd118837794af65943974ab31b24d25c
Prashant Singh <prashant@codeforamerica.org>
2012-04-09 23:18:54 -0700

    Refine the benchmarked code a little bit.

--- control.js
+++ control.js
@@ -4,14 +4,21 @@ var Benchmark = require('benchmark');
 //var fullText = "blahblah1x05blahblah";
 var fullText = "blahblah105blahblah";
 
-function a(fullText) {
+function funa1(x) {}
+function funa2(x) {}
+function funb1(x) {}
+function funb2(x) {}
+function func1(x) {}
+function func2(x) {}
+
+function test_scope_anon(fullText) {
   var num1 = null;
   var num2 = null;
   (function() {
     function testAndAssignMatch(m) {
       if (m != null) {
-        num1 = parseInt(m[1]);
-        num2 = parseInt(m[2]);
+        num1 = funa1(m[1]);
+        num2 = funa1(m[2]);
         return true;
       }
       return false;
@@ -26,19 +33,19 @@ function a(fullText) {
 }
 
 
-function b(fullText) {
+function test_if_assign(fullText) {
   var num1 = null;
   var num2 = null;
   var matches = null;
   if (matches = /[Ss](\d?\d)[Ee](\d?\d)/.exec(fullText)) {
-    num1 = parseInt(matches[1]);
-    num2 = parseInt(matches[2]);
+    num1 = funa1(matches[1]);
+    num2 = funa2(matches[2]);
   } else if (matches = /([01]?[0-9])[xX]([0-9]?[0-9])/.exec(fullText)) {
-    num1 = parseInt(matches[1]);
-    num2 = parseInt(matches[2]);
+    num1 = funb1(matches[1]);
+    num2 = funb2(matches[2]);
   } else if (matches = /(\d)\.?(\d?\d)/.exec(fullText)) {
-    num1 = parseInt(matches[1]);
-    num2 = parseInt(matches[2]);
+    num1 = func1(matches[1]);
+    num2 = func2(matches[2]);
   }
 }
 
@@ -65,48 +72,92 @@ function reSwitch() {
   }
 }
 
-function c(fullText) {
+function test_reSwitch(fullText) {
+  var num1 = null;
+  var num2 = null;
+  reSwitch(fullText
+    , /[Ss](\d?\d)[Ee](\d?\d)/
+    , function(matches) {
+        num1 = funa1(matches[1]);
+        num2 = funa2(matches[2]);
+      }
+    , /([01]?[0-9])[xX]([0-9]?[0-9])/
+    , function(matches) {
+        num1 = funb1(matches[1]);
+        num2 = funb2(matches[2]);
+      }
+    , /(\d)\.?(\d?\d)/
+    , function(matches) {
+        num1 = funa1(matches[1]);
+        num2 = func2(matches[2]);
+      }
+  );
+}
+
+function test_reSwitch_multi(fullText) {
   var num1 = null;
   var num2 = null;
   reSwitch(fullText
     , /[Ss](\d?\d)[Ee](\d?\d)/
     , /([01]?[0-9])[xX]([0-9]?[0-9])/
     , function(matches) {
-        num1 = parseInt(matches[1]);
-        num2 = parseInt(matches[2]);
+        num1 = funa1(matches[1]);
+        num2 = funa2(matches[2]);
       }
     , /(\d)\.?(\d?\d)/
     , function(matches) {
-        num1 = parseInt(matches[1]);
-        num2 = parseInt(matches[2]);
-    }
-    );
+        num1 = funa1(matches[1]);
+        num2 = func2(matches[2]);
+      }
+  );
 }
 
 function base() {
-  return;
+}
+
+function justParse() {
+  var num1 = parseInt('1');
+  var num2 = parseInt('05');
 }
 
 (function main() {
   var suite = new Benchmark.Suite;
+  var basename = 'empty function call';
+  var baseperiod = -1;
 
-  suite.add('empty function call', function() {
+  suite.add(basename, function() {
     base();
   })
+  .add('inline anonymous function call', function() {
+    (function() {return;})();
+  })
   .add('scoping function + nested function', function() {
-    a(fullText);
+    test_scope_anon(fullText);
   })
   .add('assignment inside if', function() {
-    b(fullText);
+    test_if_assign(fullText);
   })
   .add('RegExp switch meta function', function() {
-    c(fullText);
+    test_reSwitch(fullText);
+  })
+  .add('RegExp switch meta function multi', function() {
+    test_reSwitch_multi(fullText);
+  })
+  .add('just parse ints', function() {
+    justParse();
   })
   .add('simple RegExp exec', function() {
     /simple/.exec('simple');
   })
   .on('cycle', function(event, bench) {
-    console.log(String(bench));
+    if (bench.name === basename) {
+      baseperiod = bench.times.period;
+      console.log('Unit is: ' + basename);
+    }
+    console.log(bench.name);
+    console.log('Number of time units:');
+    console.log(bench.times.period / baseperiod);
+    console.log();
   })
   .run({'async': true})
 })();
